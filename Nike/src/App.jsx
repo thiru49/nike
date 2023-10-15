86842b89ff033bcf1353efa6e80156437db6407f
kaizendeveloper05 <kaizendeveloper05@gmail.com>
2023-10-16 00:04:31 +0530

    first commit

---
+++ Nike/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Hero,PopularProducts,SuperQuality,
+Service,SpecialOffer,CustomerReviews,Subscribe} from "./sections/index"
+import Nav from './components/Nav';
+
+const App = () => (
+  <main className="relative">
+    <Nav/>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero/>
+    </section>
+    <section className="padding">
+      <PopularProducts/>
+    </section>
+    <section className="padding">
+      <SuperQuality/>
+    </section>
+    <section className="padding-x py-10">
+      <Service/>
+    </section>
+    <section className="padding">
+      <SpecialOffer/>
+    </section>
+    <section className="bg-pale-blue">
+      <CustomerReviews/>
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe/>
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Subscribe
+    </section>
+  </main>
+);
+
+export default App;
---
+++ Nike/src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Service from "./Service";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+
+
+export {Hero,PopularProducts,Footer,
+CustomerReviews,Service,Subscribe,SpecialOffer,SuperQuality}
